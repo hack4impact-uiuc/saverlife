b0bcf1ba5780e6a99773dbe95b088cc3382d3da3
Siraj <sirajchokshi@gmail.com>
2020-02-16 14:20:16 -0600

    Init Calendar.js and CalendarStyles.js in components

---
+++ App/Components/Calendar/Calendar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import { Text, View } from 'react-native';
+import {styles} from './CalendarStyle'
+
+export default class Calendar extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+          Temp Text
+      </View>
+    );
+  }
+}

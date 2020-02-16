a150e6e5ebbb5c5a8259d6db71c58fa5bd8ce52c
Michael Leon <mal7@illinois.edu>
2020-02-16 14:03:15 -0600

    init chart file

---
+++ App/Components/Chart.react.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Chart extends Component {
+  render() {
+    return (
+      <p>
+        this is a chart
+      </p>
+    )
+  }
+}
+

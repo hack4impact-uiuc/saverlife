53d8ca7d1e307828fa6f7e1f6276f7765ca98fbe
Lauren Ho <laurenh4@illinois.edu>
2020-02-21 12:37:38 -0600

    Text based on props

--- App/Components/ProgressBar.js
+++ App/Components/ProgressBar.js
@@ -7,6 +7,10 @@ const styles = StyleSheet.create({
     textContainer: {
         flexDirection: 'row',
         justifyContent: 'space-between'
+    },
+    progressBar: {
+        backgroundColor: 'rgb(239, 240, 241)',
+        borderColor: 'rgb(239, 240, 241)'
     }
 });
 
@@ -14,10 +18,10 @@ class ProgressBar extends Component {
     render() {
         return(
             <View style={styles.progressContainer}>
-                <Progress.Bar color="rgb(150, 74, 246)" progress={this.props.progress} width={null} />
+                <Progress.Bar style={styles.progressBar} color="rgb(150, 74, 246)" progress={this.props.progress} width={null} />
                 <View style={styles.textContainer}>
-                    <Text>34%</Text>
-                    <Text>$ 136/400</Text>
+                    <Text>{this.props.progress * 100}%</Text>
+                    <Text>${this.props.progress * 400} / 400</Text>
                 </View>
             </View>
         );

6f1942435bd9ada905a2ee5993f2b05e505af902
Lauren Ho <laurenh4@illinois.edu>
2020-02-21 12:31:05 -0600

    Changed color of progress bar

--- App/Components/ProgressBar.js
+++ App/Components/ProgressBar.js
@@ -4,10 +4,7 @@ import { StyleSheet, View, Text } from 'react-native';
 import * as Progress from 'react-native-progress';
 
 const styles = StyleSheet.create({
-    progressContainer: {
-        backgroundColor: '#ff0000',
-    },
-    textContainer:{
+    textContainer: {
         flexDirection: 'row',
         justifyContent: 'space-between'
     }
@@ -17,7 +14,7 @@ class ProgressBar extends Component {
     render() {
         return(
             <View style={styles.progressContainer}>
-                <Progress.Bar progress={this.props.progress} width={null} />
+                <Progress.Bar color="rgb(150, 74, 246)" progress={this.props.progress} width={null} />
                 <View style={styles.textContainer}>
                     <Text>34%</Text>
                     <Text>$ 136/400</Text>

f29f5b1a134aabc50216401c9b451e08ba8b23fe
melodyfelix <harmonyfelix@gmail.com>
2020-02-24 23:32:54 -0600

    added article component and article-screen container

--- App/App.js
+++ App/App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/lib/integration/react'
 import createStore from 'App/Stores'
 import RootScreen from './Containers/Root/RootScreen'
+import ArticleScreen from './Containers/Article/ArticleScreen'
 
 const { store, persistor } = createStore()
 
@@ -21,7 +22,8 @@ export default class App extends Component {
          * @see https://github.com/rt2zz/redux-persist/blob/master/docs/PersistGate.md
          */}
         <PersistGate loading={null} persistor={persistor}>
-          <RootScreen />
+          {/* <RootScreen /> */}
+          <ArticleScreen/>
         </PersistGate>
       </Provider>
     )
---
+++ App/Components/Article.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+// import {Card} from 'react-native-shadow-cards';
+import { Card, Button, Icon } from 'react-native-elements'
+
+
+
+class Article extends Component {
+    render() {
+        return(
+            <Card image={require('../Assets/Images/TOM-Legend.png')} >
+                <Text style={{marginBottom: 10}}>
+                    Learn how to save money fast!!!
+                </Text>
+                <Button 
+                    buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0, width: "60%"}}
+                    title='Read More -->' />
+            </Card>
+        );
+    }
+}
+
+export default Article; 

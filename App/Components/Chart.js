21f39b20b50c5c9729a64d45eed984798e458259
Michael Leon <mal7@illinois.edu>
2020-02-24 22:36:32 -0600

    Added points per data entry, purple line

--- App/Components/Chart.js
+++ App/Components/Chart.js
@@ -8,44 +8,48 @@ export default class Chart extends Component {
   //constructor(props) {}
   render() {
 
-      {
-        const data = this.props.data;
-        const xLabels = lastNDays(data.length);
-        const config = {
-          line: {
-            visible: true,
-            strokeWidth: 1,
-            strokeColor: "#54a0ff"
-          },
-          area: {
-            visible: false
-          },
-          tooltip: {
-            visible: true,
-            labelFontSize: 10,
-            labelFormatter: moneyFormat
-          },
-          grid: {
-            //stepSize: 75
-            stepSize: (Math.max(data)-Math.min(data))/5
-          },
-          yAxis: {
-            labelColor: "#54a0ff",
-            labelFormatter: moneyFormat
-          },
-          xAxis: {
-            visible: true
-          },
-          insetY: 275,
-          insetX: 10
-        };
-        return (
-          <View style={{ flex: 1, justifyContent: "flex-end", alignItems: "flex-end" }}>
-            <Text style={styles.text}>Chart</Text>
-            <LineChart style={{ flex: 2 }} config={config} data={data} xLabels={xLabels}/>
-          </View>
-        );
-      }
+    const { data, xLabels } = this.props;
+    // const xLabels = lastNDays(data.length);
+    const config = {
+      line: {
+        visible: true,
+        strokeWidth: 2,
+        strokeColor: "#964af6"
+      },
+      area: {
+        visible: false
+      },
+      tooltip: {
+        visible: false,
+        labelFontSize: 10,
+        labelFormatter: moneyFormat
+      },
+      grid: {
+        visible: true,
+        stepSize: (Math.max(data)-Math.min(data))/5
+      },
+      yAxis: {
+        // labelColor: "#5822a3",
+        labelFormatter: moneyFormat
+      },
+      xAxis: {
+        visible: true
+      },
+      dataPoint: {
+        visible: true,
+        color: "#5822a3",
+        radius: 4,
+        label: { visible: true, marginBottom: 25 }
+      },
+      insetY: 10,
+      insetX: 10
+    };
+    return (
+      <View style={{ flex: 1, justifyContent: "flex-end", alignItems: "center" }}>
+        <Text style={styles.text}>Chart</Text>
+        <LineChart style={{ flex: 2 }} config={config} data={data} xLabels={xLabels}/>
+      </View>
+    );
   }
 }
 

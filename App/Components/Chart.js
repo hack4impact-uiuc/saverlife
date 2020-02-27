2aa333cffdeba0b5ce3d772f7b8b3321e12a9697
Michael Leon <mal7@illinois.edu>
2020-02-26 19:57:49 -0600

    Move LineChart config to ChartStyle.js

--- App/Components/Chart.js
+++ App/Components/Chart.js
@@ -7,40 +7,8 @@ import {styles} from'./ChartStyle';
 export default class Chart extends Component {
   render() {
     const { data, xLabels, title} = this.props;
-    const config = {
-      line: {
-        visible: true,
-        strokeWidth: 2,
-        strokeColor: "#964af6"
-      },
-      area: {
-        visible: false
-      },
-      tooltip: {
-        visible: true,
-        labelFontSize: 10,
-        labelFormatter: moneyFormat
-      },
-      grid: {
-        visible: true,
-        stepSize: (Math.max(data)-Math.min(data))/5
-      },
-      yAxis: {
-        // labelColor: "#5822a3",
-        labelFormatter: moneyFormat
-      },
-      xAxis: {
-        visible: true
-      },
-      dataPoint: {
-        visible: true,
-        color: "#5822a3",
-        radius: 4,
-        label: { visible: false, marginBottom: 25 }
-      },
-      insetY: 10,
-      insetX: 10
-    };
+    const config = styles.config;
+    config.grid.stepSize = (Math.max(data)-Math.min(data))/5;
     return (
       <View style={{ flex: 1, justifyContent: "flex-end", alignItems: "center" }}>
         <Text style={styles.text}>{title}</Text>
--- App/Components/ChartStyle.js
+++ App/Components/ChartStyle.js
@@ -5,6 +5,39 @@ export const styles = StyleSheet.create({
   text: {
     ...Fonts.normal,
     textAlign: 'center',
-    
-  }
+
+  },
+  config: {
+    line: {
+      visible: true,
+      strokeWidth: 2,
+      strokeColor: "#964af6"
+    },
+    area: {
+      visible: false
+    },
+    tooltip: {
+      visible: true,
+      labelFontSize: 10,
+      labelFormatter: moneyFormat
+    },
+    grid: {
+      visible: true
+    },
+    yAxis: {
+      // labelColor: "#5822a3",
+      labelFormatter: moneyFormat
+    },
+    xAxis: {
+      visible: true
+    },
+    dataPoint: {
+      visible: true,
+      color: "#5822a3",
+      radius: 4,
+      label: { visible: false, marginBottom: 25 }
+    },
+    insetY: 10,
+    insetX: 10
+  };
 })

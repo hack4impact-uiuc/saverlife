8dbff36fd6f7ea68c256e6714ef9d1b4529d0b38
Michael Leon <mal7@illinois.edu>
2020-02-26 21:03:49 -0600

    Fixed error in Chart.js

--- App/Components/Chart.js
+++ App/Components/Chart.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import LineChart from "react-native-responsive-linechart";
-import {styles} from'./ChartStyle';
+import {styles, lineChartConfig} from'./ChartStyle';
 
 
 export default class Chart extends Component {
   render() {
     const { data, xLabels, title} = this.props;
-    const config = styles.config;
-    config.grid.stepSize = (Math.max(data)-Math.min(data))/5;
+    lineChartConfig.grid.stepSize = (Math.max(data)-Math.min(data))/5;
     return (
       <View style={{ flex: 1, justifyContent: "flex-end", alignItems: "center" }}>
         <Text style={styles.text}>{title}</Text>
-        <LineChart style={{ flex: 2 }} config={config} data={data} xLabels={xLabels}/>
+        <LineChart style={{ flex: 2 }} config={lineChartConfig} data={data} xLabels={xLabels}/>
       </View>
     );
   }
-}
-
-function moneyFormat(amt){
-  return "$" + amt.toFixed(2);
 }

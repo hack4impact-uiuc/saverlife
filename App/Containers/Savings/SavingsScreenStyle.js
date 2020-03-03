f85640213cda765e145b70fe1430ed7b0ee8fcf1
Lauren Ho <laurenh4@illinois.edu>
2020-03-02 23:29:08 -0600

    Remove card styline from savings

--- App/Containers/Savings/SavingsScreenStyle.js
+++ App/Containers/Savings/SavingsScreenStyle.js
@@ -4,22 +4,6 @@ export default StyleSheet.create({
     savingsContainer: {
         margin: '3%',
     },
-    card: {
-        borderWidth: 0.5,
-        borderRadius: 5,
-        borderColor: '#d6d7da',
-        backgroundColor: '#FFFFFF',
-        padding: '3%',
-        marginTop: '2%',
-        marginBottom: '2%',
-        shadowColor: '#000',
-        shadowOpacity: 0.3,
-        shadowRadius: 4,
-        shadowOffset: {
-            width: 1,
-            height: 3
-        },
-    },
     chartContainer: {
         height: "45%",
     },

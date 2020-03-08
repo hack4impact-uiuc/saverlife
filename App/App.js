64cbeba89231f6fcb979eb8c71669c6821e47ed3
Matthew Walowski <mattwalowski@gmail.com>
2020-03-07 21:23:34 -0600

    Remove unused file

--- App/App.js
+++ App/App.js
@@ -4,6 +4,7 @@ import { PersistGate } from 'redux-persist/lib/integration/react'
 import createStore from 'App/Stores'
 import RootScreen from './Containers/Root/RootScreen'
 import SavingsScreen from './Containers/Savings/SavingsScreen'
+import CalendarScreen from './Containers/CalendarScreen/CalendarScreen'
 
 const { store, persistor } = createStore()
 
@@ -23,7 +24,8 @@ export default class App extends Component {
          */}
         <PersistGate loading={null} persistor={persistor}>
           {/* <RootScreen /> */}
-          <SavingsScreen />
+          {/* <SavingsScreen /> */}
+          <CalendarScreen />
         </PersistGate>
       </Provider>
     )

fe09bc0d598de7a1c5ded1a579ff8f2149712167
Michael Leon <mal7@illinois.edu>
2020-03-09 22:45:00 -0500

    init article component

---
+++ App/Components/SingleArticle.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import Style from './SingleArticleStyle';
+
+class SingleArticle extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <View>
+        <Text>Hello</Text>
+      </View>
+    );
+  }
+}
+
+export default SingleArticle;
+
